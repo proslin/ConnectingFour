de0fa5951e9696da27a8e61f7acd20f07a0e5ff9
proslin <linchik@mail.ru>
2022-09-20 16:49:12 +0300

    Part five with multiple games

--- ConnectFour/ConnectFour/main.swift
+++ ConnectFour/ConnectFour/main.swift
@@ -7,13 +7,27 @@
 
 import Foundation
 
-var firstPlayer = String()
-var secondPlayer = String()
+struct Player {
+    let name: String
+    let symbol: String
+    var isFirst: Bool
+    var score: Int = 0
+    
+    mutating func increaseScore(points: Int) {
+        self.score += points
+    }
+}
+
+var firstPlayerName = String()
+var secondPlayerName = String()
 var columns = Int()
 var rows = Int()
-let firstPlayerSymbol = "o"
-let secondPlayerSymbol = "*"
+//let firstPlayerSymbol = "o"
+//let secondPlayerSymbol = "*"
 var arr = [[String]]()
+//var score = [String: Int]()
+var totalGames = Int()
+var isEndOfGame = false
 
 enum ErrosMessage: String {
     case invalidInput = "Invalid input"
@@ -38,10 +52,11 @@ func getDimensionsFromString(_ input: String) -> (row: Int, column: Int)? {
     var rowCount : Int = 6
     var columnCount: Int = 7
     if input == "" { return (row: rowCount, column: columnCount) }
-    let filteredInput = input.lowercased().filter { $0 != " "}
+    let filteredInput = input.lowercased().filter { $0 != " " }
     if !(filteredInput.contains("x")) {
         print(ErrosMessage.invalidInput.rawValue)
-        return nil}
+        return nil
+    }
     let array = filteredInput.components(separatedBy: "x")
     if let rowStr = array.first, let columnStr = array.last {
         if let row = Int(rowStr), let column = Int(columnStr) {
@@ -85,6 +100,33 @@ func readBoardDimensions() {
     
 }
 
+func readNumberOfGames() -> Int {
+    let message = """
+          Do you want to play single or multiple games?
+          For a single game, input 1 or press Enter
+          Input a number of games:
+          """
+    var gamesNumber = Int()
+    repeat{
+        print(message)
+        let input = readLine()
+        guard let input = input else { break }
+        if input == "" {
+            gamesNumber = 1
+            break
+        }
+        guard let number = Int(input) else {
+            print(ErrosMessage.invalidInput.rawValue)
+            continue
+        }
+        
+        gamesNumber = number
+        break
+    } while true
+    
+    return gamesNumber
+}
+
 func readColumnNumber(message: String) -> Int? {
     var columnNumber = Int()
     repeat{
@@ -94,7 +136,7 @@ func readColumnNumber(message: String) -> Int? {
             continue
             }
         if isEnd(input: input) {
-            
+            isEndOfGame = true
             return nil
         }
         guard let columnNum = Int(input) else {
@@ -113,14 +155,16 @@ func readColumnNumber(message: String) -> Int? {
 
 func startGame() {
     print("Connect Four")
-    firstPlayer = readInput(message: "First player's name \("\n") >")
-    secondPlayer = readInput(message: "Second player's name")
+    firstPlayerName = readInput(message: "First player's name")
+    secondPlayerName = readInput(message: "Second player's name")
     readBoardDimensions()
+    totalGames = readNumberOfGames()
+    totalGames == 1 ? print("Single game") : print("Total \(totalGames) games")
     
 }
 
 func printStartedParam() {
-    print("\(firstPlayer) VS \(secondPlayer)")
+    print("\(firstPlayer.name) VS \(secondPlayer.name)")
     print("\(rows) X \(columns) board")
 }
 
@@ -135,6 +179,9 @@ func printGameBoard(row: Int, column: Int) {
     print(" \(str)")
 }
 
+func initEpptyArray(row: Int, column: Int) {
+   arr = [[String]](repeating: [String](repeating: " ", count: column), count: row)
+}
 
 func printArray(arr: [[String]]) {
     for i in (0...(rows - 1 )) {
@@ -162,29 +209,29 @@ func findRowNumber(colNum: Int) -> Int? {
     }
     return nil
 }
+func getSymbolForCheck(symbol: String) -> String {
+    return (symbol + symbol + symbol + symbol)
+}
 
 func checkHorisontal(row: Int, symbol: String) -> Bool {
     var str = String()
-    let symbolForCheck = symbol + symbol + symbol + symbol
     str = arr[row].joined()
-    if str.contains(symbolForCheck) { return true }
+    if str.contains(getSymbolForCheck(symbol: symbol)) { return true }
     return false
 }
 
 func checkVertical(column: Int, symbol: String) -> Bool {
     var str = String()
-    let symbolForCheck = symbol + symbol + symbol + symbol
     for j in (0...rows - 1) {
         str += arr[j][column - 1]
     }
-    if str.contains(symbolForCheck) { return true }
+    if str.contains(getSymbolForCheck(symbol: symbol)) { return true }
     return false
 }
 
 func checkDiagonal(column: Int, row: Int, symbol: String) -> Bool {
     var strUp = String()
     var strDown = String()
-    let symbolForCheck = symbol + symbol + symbol + symbol
     for i in (row - 3...row + 3) {
         for j in (column - 3...column + 3) {
             if i >= 0 && j >= 0 && i < rows && j < columns{
@@ -199,7 +246,7 @@ func checkDiagonal(column: Int, row: Int, symbol: String) -> Bool {
             }
         }
     }
-    if strUp.contains(symbolForCheck) || strDown.contains(symbolForCheck) { return true }
+    if strUp.contains(getSymbolForCheck(symbol: symbol)) || strDown.contains(getSymbolForCheck(symbol: symbol)) { return true }
     return false
 }
 
@@ -218,66 +265,88 @@ func isBoardFull(arr: [[String]]) -> Bool{
 }
 
 func game() {
-    var player : String
+    var player = String()
     var i : Int = 1
+    
     var symbol: String
-   // var col = readColumnNumber(message: "\(firstPlayer) turn:")
     repeat {
-        if (i % 2 != 0) { player = firstPlayer } else { player = secondPlayer }
+        
+        if firstPlayer.isFirst {
+            if (i % 2 != 0) {
+                player = firstPlayer.name
+            } else {
+                player = secondPlayer.name
+            }
+        } else if secondPlayer.isFirst {
+            if (i % 2 != 0) {
+                player = secondPlayer.name
+            } else {
+                player = firstPlayer.name
+            }
+        }
+        
     guard let colNum = readColumnNumber(message: "\(player) turn:") else { return }
     let columnNumber = colNum
-   // print(columnNumber)
-    //print(findRowNumber(colNum: columnNumber))
         guard let rowNumber = findRowNumber(colNum: columnNumber) else {
             print("\(columnNumber) \(ErrosMessage.fullColumn.rawValue)")
             continue
         }
     
-        if player == firstPlayer {
-        arr[rowNumber][columnNumber - 1] = firstPlayerSymbol
-            symbol = firstPlayerSymbol
+        if player == firstPlayer.name {
+            arr[rowNumber][columnNumber - 1] = firstPlayer.symbol
+            symbol = firstPlayer.symbol
         } else {
-            arr[rowNumber][columnNumber - 1] = secondPlayerSymbol
-            symbol = secondPlayerSymbol
+            arr[rowNumber][columnNumber - 1] = secondPlayer.symbol
+            symbol = secondPlayer.symbol
         }
-   printGameBoard(row: rows, column: columns)
-        // проверяем есть ли 4 одинаковых символа в ряд
-        if checkHorisontal(row: rowNumber, symbol: symbol) {
-            print("\(player) wins")
-            break
-        }
-        // проверяем есть ли 4 одинаковых символа в столбце
-        if checkVertical(column: columnNumber, symbol: symbol) {
-            print("\(player) wins")
-            break
-        }
-        
-        if checkDiagonal(column: columnNumber - 1, row: rowNumber, symbol: symbol){
-            print("\(player) wins")
-            //print("Game over")
+        printGameBoard(row: rows, column: columns)
+
+        if checkHorisontal(row: rowNumber, symbol: symbol) || checkVertical(column: columnNumber, symbol: symbol) || checkDiagonal(column: columnNumber - 1, row: rowNumber, symbol: symbol) {
+            print("\(player) won")
+            if player == firstPlayer.name {
+                firstPlayer.increaseScore(points: 2)
+            } else if player == secondPlayer.name {
+                secondPlayer.increaseScore(points: 2)
+            }
+            firstPlayer.isFirst.toggle()
+            secondPlayer.isFirst.toggle()
             break
         }
         
         if isBoardFull(arr: arr) {
             print("It is a draw")
+            firstPlayer.increaseScore(points: 1)
+            secondPlayer.increaseScore(points: 2)
+            firstPlayer.isFirst.toggle()
+            secondPlayer.isFirst.toggle()
             break
         }
         i += 1
         
     } while true
     
-//    repeat{
-//       print("\(firstPlayer) turn:")
-//        input = readLine() ?? ""
-//    } while isEnd(input: input)
-//    printGameBoard(row: rows, column: columns)
 }
 
-startGame()
-printStartedParam()
-arr = [[String]](repeating: [String](repeating: " ", count: columns), count: rows)
-printGameBoard(row: rows, column: columns)
 
-game()
+var firstPlayer : Player
+var secondPlayer : Player
+startGame()
+firstPlayer = Player(name: firstPlayerName, symbol: "0", isFirst: true, score: 0)
+secondPlayer = Player(name: secondPlayerName, symbol: "*", isFirst: false, score: 0)
+printStartedParam()
+
+for i in (1...totalGames) {
+    if totalGames > 1 {
+        print("Game #\(i)")
+    }
+    initEpptyArray(row: rows, column: columns)
+    printGameBoard(row: rows, column: columns)
+    game()
+    if isEndOfGame { break }
+    if totalGames > 1 {
+        print("Score")
+        print("\(firstPlayer.name):\(firstPlayer.score) \(secondPlayer.name):\(secondPlayer.score)")
+    }
+}
 print("Game over")
 
